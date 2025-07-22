8198ba213572b03dbccbc7e731e2158c9d7ba64f
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-22 22:51:32 +0300

    Question 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Assignment
+﻿using Assignment.Structs;
+using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             //Question (1)
+            //    Define a struct "Person" with properties "Name" and "Age".
+            //    Create an array of three "Person" objects and populate it with data.Then,
+            //    write a C# program to display the details of all the persons in the array.
+            Person[] people = 
+            {
+            new Person("Ahmed", 22),
+            new Person("Omar", 25),
+            new Person("Ali", 23)
+            };
+          
+            Console.WriteLine("Persons Details:");
+            foreach (Person person in people)
+            {
+                person.Print();
+            }
         }
     }
 }
