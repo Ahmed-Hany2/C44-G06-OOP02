e7564ff44c3e805093ad9e4842c70496e685eb8d
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-22 23:26:28 +0300

    Struct Example 02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,7 +6,28 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            
+            #region Struct Example 02
+            Point p1 = new Point(3, 4);
+            Console.WriteLine($"Point 1: ({p1.X}, {p1.Y})");
+            Point p2 = new Point(6, 8);
+            Console.WriteLine($"Point 2: ({p2.X}, {p2.Y})");
+
+            p2 = p1;
+
+            Console.WriteLine($"======================= After =====================");
+
+            Console.WriteLine($"Point 1: ({p1.X}, {p1.Y})");
+            Console.WriteLine($"Point 2: ({p2.X}, {p2.Y})");
+
+            p1.X = 100;
+            p1.Y = 200;
+
+            Console.WriteLine($"======================= After Changing =====================");
+            Console.WriteLine($"Point 1: ({p1.X}, {p1.Y})");
+            Console.WriteLine($"Point 2: ({p2.X}, {p2.Y})");
+            #endregion
+
+
         }
     }
 }
