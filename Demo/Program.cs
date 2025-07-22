8c2055d7b5031c3a7652bfc8345371b3b6c37ea8
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-22 23:22:12 +0300

    video 1 (Struct Example 01)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Demo
+﻿using System.Drawing;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
